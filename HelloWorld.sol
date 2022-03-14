d5675afe31b001fa0957243a395277afa2f79a57
phranq <74039356+franmoh@users.noreply.github.com>
2022-03-14 13:08:57 +0100

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.11;
 
 contract HelloWorld {
     string public user;
-    string public prefix = "Hello, ";
+    string public prefix = "Hello ";
     string public surfix = ", and welcome to your first Smart Contract on Ethereum!";
 
     constructor (string memory startName){
